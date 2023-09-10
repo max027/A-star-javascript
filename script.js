a61d74efe5038e67d2466afaf5ef10b2f3ec7be6
saurabh wajage <saurabhwajage887@gmail.com>
2023-09-10 21:15:28 +0530

    created maze

--- script.js
+++ script.js
@@ -3,8 +3,24 @@
   * return:none
   * description:generates blocks for pathfinding algo
 */
-const generateBlocks=()=>{
-  return Math.floor((Math.random()*(20-2)+2))
+ 
+const generateBlocks=(grid)=>{
+  let blocks=[]
+  while(blocks.length<150){
+    const num1=Math.floor(Math.random()*19-1)+1;
+    const num2=Math.floor(Math.random()*19-1)+1;
+    if((num1==0&&num2==0)||(num1==19&&num2==19)){
+      continue
+    }else{
+      blocks.push([num1,num2])
+    }
+
+  }   
+   
+
+  for(let i=0;i<blocks.length;i++){
+    grid.rows[blocks[i][0]].cells[blocks[i][1]].bgColor='black'
+  }
 }
 
 
@@ -13,8 +29,7 @@ const generateBlocks=()=>{
 *description:handels clicks
 */
 const handelClick=(cell)=>{
-  cell.bgColor='black'
-  console.log(generateBlocks())
+  console.log(cell)
 }
 
 /* args:none
@@ -30,12 +45,14 @@ let tr=grid.appendChild(document.createElement('tr'))
   for(let y=0;y<20;y++){
     let td=tr.appendChild(document.createElement('td'))
     td.innerHTML=++i
+
+/*
     td.addEventListener('click',(function(cell){
       return function(){
         handelClick(cell)
       }
     })(td),false )
-
+*/
   }
 }
 return grid
@@ -54,7 +71,8 @@ function main() {
   grid.childNodes[0].cells[0].bgColor='orange'
   grid.childNodes[19].cells[19].bgColor='green'
 
-//  console.log(grid.childNodes[0].cells[0])
+  generateBlocks(grid)
+// console.log(grid.childNodes[0].cells[0])
   document.body.appendChild(grid)
 
 }
